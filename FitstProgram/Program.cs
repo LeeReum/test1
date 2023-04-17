838c6d97881b9811fd677e404a9a259dcdd767db
hs Kim <hs2014kim@naver.com>
2023-04-17 20:09:12 +0900

    first commit

---
+++ FitstProgram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Xml.Linq;
+
+namespace FitstProgram
+{
+    class SDA
+    {
+        public int[] SelectEven(int[] input)
+        {
+            return (from item in input
+                    where item % 2 == 0
+                    select item).ToArray<int>();
+        }
+
+        public List<int> SelectEven2(List<int> input)
+        {
+            return (from item in input
+                    where item % 2 == 0
+                    select item).ToList<int>();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
